1033e862975f21d0509420525ac3473ba985c17b
hambalee <hambalee@outlook.com>
2022-09-14 11:02:16 +0700

    connect mongodb database

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./config.js')
 
 const app = express();
 const PORT = 8080
 
+const connect = async () => {
+    try {
+        await mongoose.connect(config.mongo);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("Disconnected from MongoDB");
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB connected");
+});
+
+app.get('/', (req,res)=> {
+    console.log("hello world")
+    res.send("Hi")
+})
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
 })
+
+connect();
