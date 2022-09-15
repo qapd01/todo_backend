9a364da79f6e75761e28b183edbe06aac2b795b7
Sorawid Mingmolee <bossxlp@gmail.com>
2022-09-15 11:09:13 +0700

    change path Atlas to localhost

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-
 const mongoose = require("mongoose");
-const config = require('./config')
-const app = require('./api/index')
+const config = require("./config");
+const app = require("./api/index");
 
-
-const PORT = require('./config')
-
+const PORT = require("./config");
 
 const connect = async () => {
   try {
@@ -23,7 +20,6 @@ mongoose.connection.on("disconnected", () => {
 mongoose.connection.on("connected", () => {
   console.log("MongoDB connected");
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
